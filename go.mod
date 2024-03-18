d845f39a03e11afaff078505bbb3700b0ee3bcca
priyank-amagi <priyank@amagi.com>
2024-03-18 16:15:20 +0530

    tests udpated

--- go.mod
+++ go.mod
@@ -6,11 +6,14 @@ require github.com/spf13/cobra v1.8.0
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/gorm v1.25.7 // indirect
+	gotest.tools v2.2.0+incompatible // indirect
 )
--- pkg/todos/todos_test.go
+++ pkg/todos/todos_test.go
@@ -1,36 +1,51 @@
 package todos
 
-// func TestNewTodoSvc(t *testing.T) {
-// 	testHost := "test_json_host_name"
-// 	assert.Equal(t, NewTodoSvc(testHost), &TodoSvc{hostname: testHost})
-// }
+import (
+	"net/http"
+	"testing"
 
-// func TestGetTodos(t *testing.T) {
-// 	tests := []struct {
-// 		name  string
-// 		host  string
-// 		todos []Todo
-// 		err   error
-// 	}{
-// 		{
-// 			name: "Invalid schema",
-// 			host: "test_json_host",
-// 			err:  fmt.Errorf("Get \"/todos\": unsupported protocol scheme \"\""),
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		todoSvc := NewTodoSvc(tt.host)
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			todos, err := todoSvc.GetTodos()
-// 			if tt.err == nil {
-// 				assert.Equal(t, tt.todos, todos)
-// 			} else {
-// 				if err == nil {
-// 					t.Error("Expected error, got nil")
-// 				} else {
-// 					assert.Equal(t, tt.err.Error(), err.Error())
-// 				}
-// 			}
-// 		})
-// 	}
-// }
+	"github.com/priyank-amagi/toolbox/lib/http_helper"
+	http_mocks "github.com/priyank-amagi/toolbox/lib/http_helper/mocks"
+	"gotest.tools/assert"
+)
+
+func TestNewTodoSvc(t *testing.T) {
+	httpSvc := http_helper.NewHttpSvc()
+	testHost := "test_json_host_name"
+	assert.Equal(t, *NewTodoSvc(testHost, httpSvc), TodoSvc{hostname: testHost, httpSvc: httpSvc})
+}
+
+// TODO: this tests are incomplete
+func TestGetTodos(t *testing.T) {
+	httpSvc := http_mocks.NewIHttpSvc(t)
+	tests := []struct {
+		name          string
+		host          string
+		todos         []Todo
+		request       *http.Request
+		response      *http.Response
+		expectedError string
+	}{
+		{
+			name:          "Invalid URL",
+			host:          "://invalid-url",
+			expectedError: "parse \"://invalid-url\": missing protocol scheme",
+		},
+	}
+	for _, tt := range tests {
+		todoSvc := NewTodoSvc(tt.host, httpSvc)
+		t.Run(tt.name, func(t *testing.T) {
+			todos, err := todoSvc.GetTodos()
+			if tt.expectedError != "" {
+				if err == nil || err.Error() != tt.expectedError {
+					t.Errorf("Expected error: %s, got: %v", tt.expectedError, err)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Unexpected error: %v", err)
+				}
+				assert.Equal(t, tt.todos, todos)
+			}
+		})
+	}
+}
